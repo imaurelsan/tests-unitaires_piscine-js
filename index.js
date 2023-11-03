b20cc30d97af268d51b7f69caa126952c13044b5
Aure Yahouedeou <admofficiel@gmail.com>
2023-11-03 16:46:28 +0100

    ok

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const chevauchementDates = require('./s2/exercice9')
 const calculerAge = require('./s2/exercice10')
 
 const PORT = 3000;
-
+app.use(express.json())
 app.get('/', (req, res) => {
     return res.json([{ id: 1, name: 'John Doe' }]);
 });
@@ -32,16 +32,43 @@ app.post('/:param', (req, res) => {
     return res.json([{ error: false, num: req.params.param }]);
 });
 
-app.post('/s1/exercice1', (req, res) => {
-    const n1 = req.body.n1
-    const n2 = req.body.n2
-    let a = addition(n1, n2)
-    return res.json([{ reponse: a }]);
-});
+app.post("/s1/exercice1", (req, res) => { // CECI EST UNE ROUTE : LA ROUTE DE L'EXERCICE 1 EN GROS...
+    const n1 = req.body.n1;
+    const n2 = req.body.n2;
+  
+    // On intègre tous les cas d'erreurs à tester ensuite dans l'index.test.js
+    if (n1 == null || n2 == null) {
+      return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+
+    if (!n1) {
+      return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+  
+    if (!n2) {
+      return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+  
+    let a = addition(n1, n2);
+    return res.status(200).json([{ reponse: a }]);
+  });
 
 app.post('/s1/exercice2', (req, res) => {
     const n1 = req.body.n1
     const n2 = req.body.n2
+
+    if (n1 == null || n2 == null) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+  
+      if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+      }
+    
+      if (!n2) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+      }
+
     let a = soustraction(n1, n2)
     return res.json([{ reponse: a }]);
 });
@@ -49,6 +76,16 @@ app.post('/s1/exercice2', (req, res) => {
 app.post('/s1/exercice3', (req, res) => {
     const n1 = req.body.n1
     const n2 = req.body.n2
+    if (n1 == null || n2 == null) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+      if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+      }
+      if (!n2) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+      }
+
     let a = multiplication(n1, n2)
     return res.json([{ reponse: a }]);
 });
@@ -56,36 +93,87 @@ app.post('/s1/exercice3', (req, res) => {
 app.post('/s1/exercice4', (req, res) => {
     const n1 = req.body.n1
     const n2 = req.body.n2
+    if (n1 == null || n2 == null) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+      if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+      }
+      if (!n2) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+      }
+
     let a = division(n1, n2)
     return res.json([{ reponse: a }]);
 });
 
 app.post('/s1/exercice5', (req, res) => {
     const n1 = req.body.n1
+    if (n1 === 0 || n1 === 1) {
+        return res.json([{ reponse: "Factorielle de 0 ou 1 doit être égale à 1" }]);
+    }
+    if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+
     let a = factorielle(n1)
     return res.json([{ reponse: a }]);
 });
 
 app.post('/s1/exercice6', (req, res) => {
     const n1 = req.body.n1
+    if (n1 === 2) {
+        return res.json([{ reponse: "Le nombre est paire" }]);
+    }
+    if (n1 === 3) {
+        return res.json([{ reponse: "Le nombre est impaire" }]);
+    }
+    if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+    if (n1 == null) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+   
     let a = estPairOuImpair(n1)
     return res.json([{ reponse: a }]);
 });
 
 app.post('/s1/exercice7', (req, res) => {
     const n1 = req.body.n1
+    if (n1 == null) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+    if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+
     let a = celsiusEnFahrenheit(n1)
     return res.json([{ reponse: a }]);
 });
 
 app.post('/s1/exercice8', (req, res) => {
     const n1 = req.body.n1
+    if (n1 == null) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+    if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+
     let a = aireCercle(n1)
     return res.json([{ reponse: a }]);
 });
 
 app.post('/s1/exercice9', (req, res) => {
     const n1 = req.body.n1
+    if (n1 == "") {
+        return res.json([{ reponse: "Veuillez entrer un mot valide" }]);
+    }
+    if (!n1) {
+        return res.json([{ reponse: "Veuillez entrer un mot valide" }]);
+    }
+
     let a = estPalindrome(n1)
     return res.json([{ reponse: a }]);
 });
@@ -93,6 +181,22 @@ app.post('/s1/exercice9', (req, res) => {
 app.post('/s1/exercice10', (req, res) => {
     const n1 = req.body.n1
     const n2 = req.body.n2
+    if (n1 === 0 && n2 === 0) {
+        return res.json([{ reponse: 0 }]);
+    }
+    if (n1 === 0 || n2 === 0) {
+        return res.json([{ reponse: Math.abs(n1 || n2) }]);
+    }
+    if (n1 === n2) {
+        return res.json([{ reponse: n1 }]);
+    }
+    if (n1 == null || n2 == null) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+    if (!n1 || !n2) {
+        return res.json([{ reponse: "Veuillez entrer un nombre valide" }]);
+    }
+
     let a = pgcd(n1, n2)
     return res.json([{ reponse: a }]);
 });
--- index.test.js
+++ index.test.js
@@ -26,3 +26,315 @@ describe('POST /:param', () => {
             });
     });
 });
+
+
+describe('POST /s1/exercice1', () => {
+    // ICI ON VA TESTER LA PREMIERE CONDITION OÙ n1 OU n2 SERAIT NULL 
+    test('Test addition', async () => { // async permet d'utiliser le await
+        const res = await request(app) // Ici on met await pour lui dire d'exécuter ceci avant d'exécuter les expect
+            .post('/s1/exercice1')
+            .send({ n1: 2, n2: 3 }) 
+                
+        expect(res.statusCode).toEqual(200); // Ici je teste que le statut est 200 (succès)
+        expect(res.body).toEqual([{reponse : 5}]) // Ici je vérifie la réponse attendue qui est 5
+    });
+
+    // DANS LE MEME DESCRIBE ON TESTE QUAND n1 EST NULL
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice1')
+            .send({n1: null, n2: 3})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+
+    // DANS LE MEME DESCRIBE ON TESTE QUAND n2 EST NULL
+    test('Test n2 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice1')
+            .send({n1: 2, n2: null})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
+
+describe('POST /s1/exercice2', () => {
+    // ICI ON VA TESTER LA PREMIERE CONDITION OÙ n1 OU n2 SERAIT NULL 
+    test('Test soustraction', async () => { // async permet d'utiliser le await
+        const res = await request(app) // Ici on met await pour lui dire d'exécuter ceci avant d'exécuter les expect
+            .post('/s1/exercice2')
+            .send({ n1: 3, n2: 2 }) 
+                
+        expect(res.statusCode).toEqual(200); // Ici je teste que le statut est 200 (succès)
+        expect(res.body).toEqual([{reponse : 1}]) // Ici je vérifie la réponse attendue qui est 5
+    });
+
+    // DANS LE MEME DESCRIBE ON TESTE QUAND n1 EST NULL
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice2')
+            .send({n1: null, n2: 2})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+
+    // DANS LE MEME DESCRIBE ON TESTE QUAND n2 EST NULL
+    test('Test n2 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice2')
+            .send({n1: 3, n2: null})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
+
+describe('POST /s1/exercice3', () => {
+    test('Test multiplication', async () => {
+        const res = await request(app)
+            .post('/s1/exercice3')
+            .send({ n1: 8, n2: 3 }) 
+                
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : 24}])
+    });
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice3')
+            .send({n1: null, n2: 3})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n2 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice3')
+            .send({n1: 8, n2: null})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
+
+describe('POST /s1/exercice4', () => {
+    test('Test division', async () => {
+        const res = await request(app)
+            .post('/s1/exercice4')
+            .send({ n1: 6, n2: 2 }) 
+                
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : 3}])
+    });
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice4')
+            .send({n1: null, n2: 2})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n2 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice4')
+            .send({n1: 6, n2: null})
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n1 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice4')
+            .send({n1: undefined})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n2 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice4')
+            .send({n2: undefined})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
+
+describe('POST /s1/exercice5', () => {
+    test('Test factorielle de 0', async () => {
+        const res = await request(app)
+            .post('/s1/exercice5')
+            .send({ n1: 0 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Factorielle de 0 ou 1 doit être égale à 1"}])
+    });
+    test('Test factorielle de 1', async () => {
+        const res = await request(app)
+            .post('/s1/exercice5')
+            .send({ n1: 1 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Factorielle de 0 ou 1 doit être égale à 1"}])
+    });
+    test('Factorielle', async () => {
+        const res = await request(app)
+            .post('/s1/exercice5')
+            .send({ n1: 5 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : 120}])
+    });
+});
+
+describe('POST /s1/exercice6', () => {
+    test('Test nombre Paire', async () => {
+        const res = await request(app)
+            .post('/s1/exercice6')
+            .send({ n1: 2 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Le nombre est paire"}])
+    });
+    test('Test nombre Impaire', async () => {
+        const res = await request(app)
+            .post('/s1/exercice6')
+            .send({ n1: 3 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Le nombre est impaire"}])
+    });
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice7')
+            .send({n1: null})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n1 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice7')
+            .send({n1: undefined})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
+
+describe('POST /s1/exercice7', () => {
+    test('Test Conversion de Celsius à Fahrenheit', async () => {
+        const res = await request(app)
+            .post('/s1/exercice7')
+            .send({ n1: 2 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : 35.6}])
+    });
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice7')
+            .send({n1: null})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n1 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice7')
+            .send({n1: undefined})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
+
+describe('POST /s1/exercice8', () => {
+    test('Test Calcul de laire dun cercle', async () => {
+        const res = await request(app)
+            .post('/s1/exercice8')
+            .send({ n1: 4 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : 50.26548245743669}])
+    });
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice8')
+            .send({n1: null})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n1 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice8')
+            .send({n1: undefined})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
+
+describe('POST /s1/exercice9', () => {
+    test('Test de vérification de palindrome positif', async () => {
+        const res = await request(app)
+            .post('/s1/exercice9')
+            .send({ n1: "kayak" }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : true}])
+    });
+    test('Test de vérification de palindrome négatif', async () => {
+        const res = await request(app)
+            .post('/s1/exercice9')
+            .send({ n1: "allan" }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : false}])
+    });
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice9')
+            .send({n1: ""})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un mot valide"}])
+    });
+    test('Test n1 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice9')
+            .send({n1: undefined})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un mot valide"}])
+    });
+});
+
+describe('POST /s1/exercice10', () => {
+    test('Test de Calcul du PGCD de deux nombres premiers', async () => {
+        const res = await request(app)
+            .post('/s1/exercice10')
+            .send({ n1: 7, n2: 11 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : 1}])
+    });
+    test('Test n1 = n2', async () => {
+        const res = await request(app)
+            .post('/s1/exercice10')
+            .send({ n1: 15, n2: 15 }) 
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : 15}])
+    });
+    test('Test n1 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice10')
+            .send({n1: null})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n2 est null', async () => {
+        const res = await request(app)
+            .post('/s1/exercice10')
+            .send({n2: null})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n1 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice10')
+            .send({n1: undefined, n2:7})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+    test('Test n2 est undefined', async () => {
+        const res = await request(app)
+            .post('/s1/exercice10')
+            .send({n1:11, n2: undefined})
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual([{reponse : "Veuillez entrer un nombre valide"}])
+    });
+});
